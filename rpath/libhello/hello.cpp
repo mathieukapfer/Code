cb2e467450bdc2a4120e10271308af338abb305b
Kapfer Family <kapfer@debian-desktop>
2021-03-03 08:45:38 +0100

    [rpath] fix

--- rpath/libhello/hello.cpp
+++ rpath/libhello/hello.cpp
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#include "hello.h"
+
 void sayHello() {
   printf("Hello\n");
 }
--- rpath/main.cpp
+++ rpath/main.cpp
@@ -1,8 +1,8 @@
-#include <stdio.h>
 #include "hello.h"
 
 int main(int argc, char *argv[])
 {
+  sayHello();
 
   return 0;
 }

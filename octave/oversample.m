7faa77a4fa04a2733fb9397b28b1d7423c1de717
Kapfer Family <kapfer@debian-desktop>
2022-11-18 08:16:09 +0100

    [octave] oversampling

---
+++ octave/oversample.m
@@ -0,0 +1,52 @@
+function oversample()
+
+% original resolution
+res = 1/20
+nb_period = 4
+
+% original wave
+x = [-nb_period/2:res:nb_period/2-res];
+y = the_function(x);
+%y = carre(x);
+
+% resample wave
+y_resample = reshape(resize(y,2,size(y,2)),1,2*size(y,2));
+x_resample=[1:1:2*size(y,2)];
+
+% display both wave
+x_=[1:1:size(y,2)]; % = original index
+
+figure
+
+subplot(3,1,1)
+hold on
+plot(1/2+x_resample/2,y_resample)
+plot(x_,y)
+
+subplot(3,1,2)
+hold on
+plot(x_resample - size(x_resample,2)/2, abs(fft(y_resample)))
+plot(x_ - size(x_,2)/2, abs(fft(y)))
+
+subplot(3,1,3)
+hold on
+plot(real(ifft(fft(y_resample))))
+
+
+hold off
+
+function y = the_function(x)
+    y = sin(2*pi*x);
+end
+
+function y = carre(x)
+    base = 5
+    x_size=size(x,2)
+    y = zeros(x_size, 1);
+    for i = [x_size/2- base: x_size/2+base]
+        y(i) = 1;
+    end
+end
+
+
+end

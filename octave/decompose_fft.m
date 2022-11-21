27d23329ddde3deaf0916e53a70208cf8a0c80d9
Kapfer Family <kapfer@debian-desktop>
2022-11-21 08:53:20 +0100

    [octave] improv decompose fft

--- octave/decompose_fft.m
+++ octave/decompose_fft.m
@@ -1,24 +1,35 @@
 function decompose_fft(k)
 
-N=200 % nb echnatillon
-T=100 % period
+N=20 % nb echnatillon
+T=10 % period
 range=0:1:N-1;
-y = cos(2*pi*range/T);
+y = 1*cos(2*pi*range/T);
 
 M = coef_fft(k,N);
 
-subplot(3,1,1)
+subplot(5,1,1)
 plot(y,'o-')
 
-subplot(3,1,2)
+subplot(5,1,2)
 plot(real(M),'o-')
 
-subplot(3,1,3)
+subplot(5,1,3)
 plot(y.*M,'o-')
 sum = y*M'
 title(sprintf("sym: real=%.1f, img=%.1f, module=%.1f", real(sum), imag(sum), abs(sum)))
 
-fft(y)(k)
+dft=fft(y);
+subplot(5,1,4)
+plot(abs(dft),'o-')
+hold on
+plot(k, abs(dft(k)),'o')
+title("dft")
+hold off
+
+subplot(5,1,5)
+plot(real(ifft(resize(dft(1:10),1,20))),'o-')
+hold on
+plot(real(ifft(dft)),'o-')
 
 function M = coef_fft(k,N)
     for n = 1 :N,

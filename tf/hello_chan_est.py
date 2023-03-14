896ae6da362bdafdea79fd90c0d5a97f7c8f7db0
Mathieu Kapfer <mkapfer@kalrayinc.com>
2023-03-14 18:34:42 +0100

    [tf] chan est update

--- tf/hello_chan_est.py
+++ tf/hello_chan_est.py
@@ -1,12 +1,19 @@
 import tensorflow as tf
 import scipy.io
-import numpy
+import numpy as np
 import mat73
 import matplotlib.pyplot as plt
 
+# script params
+# =================================================================
+training = False
+evaluate = True
+traindata_size = 256
+nb_input_depth = 2   # value '2' means one layer with real and complex value
+weight_file = "chan_test_25600"
 
-nb_input_depth = 2 # value '2' means one layer with real and complex value
-
+# =================================================================
+# cnn definition
 input_shape = (1, 612, 14, nb_input_depth)
 x = tf.random.normal(input_shape)
 
@@ -24,10 +31,12 @@ print(y.shape)
 print(y.dtype)
 
 # read mat files
-#trainDataMat = scipy.io.loadmat("chan_est_trainData2.mat")
-#trainLabelsMat = scipy.io.loadmat("chan_est_trainLabels2.mat")
-trainDataMat = mat73.loadmat("trainData_25600.mat")
-trainLabelsMat = mat73.loadmat("trainLabels_25600.mat")
+if traindata_size <= 25600:
+    trainDataMat = scipy.io.loadmat(f"trainData_{traindata_size}.mat")
+    trainLabelsMat = scipy.io.loadmat(f"trainLabels_{traindata_size}.mat")
+else:
+    trainDataMat = mat73.loadmat(f"trainData_{traindata_size}.mat")
+    trainLabelsMat = mat73.loadmat("trainLabels_{traindata_size}.mat")
 
 # get batch lenght i.e. last column
 batchLen = len(trainDataMat['trainData'][1][1][1])
@@ -39,10 +48,10 @@ x_test = trainDataMat['trainData'][:,:,:,batchLen//2+1:batchLen]
 y_test = trainLabelsMat['trainLabels'][:,:,:,batchLen//2+1:batchLen]
 
 # move batch column first
-x_train_t =  numpy.transpose(x_train,[3,0,1,2])
-y_train_t =  numpy.transpose(y_train,[3,0,1,2])
-x_test_t =  numpy.transpose(x_test,[3,0,1,2])
-y_test_t =  numpy.transpose(y_test,[3,0,1,2])
+x_train_t =  np.transpose(x_train,[3,0,1,2])
+y_train_t =  np.transpose(y_train,[3,0,1,2])
+x_test_t =  np.transpose(x_test,[3,0,1,2])
+y_test_t =  np.transpose(y_test,[3,0,1,2])
 
 print(x_train_t.shape)
 print(y_train_t.shape)
@@ -61,32 +70,82 @@ if nb_input_depth == 1:
     print(x_test_r.shape)
     print(y_test_r.shape)
 
+
 # compile
 model.compile(optimizer='adam',
               loss=tf.keras.losses.MeanSquaredError(),
-              metrics=['accuracy'])
+              metrics=[tf.keras.metrics.MeanSquaredError()])
 
-if nb_input_depth == 1:
-    # work with real (or imaginary) part
-    # train
-    model.fit(x_train_r, y_train_r, epochs=5)
-    # test
-    model.evaluate(x_test_r, y_test_r)
+# display model information
+model.summary()
+
+# train or execute
+if training:
+    print("Training in progress\n")
+    # fit
+    if nb_input_depth == 1:
+        # work with real (or imaginary) part
+        # train
+        model.fit(x_train_r, y_train_r, epochs=5)
+    else:
+        # work with complex value
+        # train
+        model.fit(x_train_t, y_train_t, validation_data=(x_test_t, y_test_t),  epochs=5 )
+
+    model.save(weight_file)
 else:
-    # work with complex value
-    # train
-    model.fit(x_train_t, y_train_t, validation_data=(x_test_t, y_test_t),  epochs=5 )
-    # test
-    model.evaluate(x_test_t, y_test_t)
+    print("Load previous weight\n")
+    model.load_weights(weight_file)
+
+# evaluate
+if evaluate:
+    print("Evaluate")
+    if nb_input_depth == 1:
+        # test
+        model.evaluate(x_test_r, y_test_r)
+    else:
+        model.fit(x_train_t, y_train_t, validation_data=(x_test_t, y_test_t),  epochs=1 )
+        # test
+        model.evaluate(x_test_t, y_test_t)
 
 
 # display one example
-num=3000
-plt.figure()
-plt.subplot(131)
-plt.imshow(x_test[:,:,0,num], cmap='hot', extent = [0, 14, 0, 20])
-plt.subplot(132)
-plt.imshow(y_test[:,:,0,num], cmap='hot', extent = [0, 14, 0, 20])
-plt.subplot(133)
-plt.imshow(model(x_test_t[num:num+1,:,:,:])[0,:,:,0], cmap='hot', extent = [0, 14, 0, 20])
-plt.show(block=False)
+for num in range(x_test[0,0,0].shape[0]):
+    plt.figure()
+
+    # display grid
+    plt.subplot(141)
+    plt.title("input grid")
+    plt.imshow(x_test[:,:,0,num], cmap='hot', extent = [0, 14, 0, 20])
+
+    # compute model estimation
+    m_test = model(x_test_t[num:num+1,:,:,:])[0,:,:,0]
+
+    # get range
+    _min = min(np.min(y_test[:, :, 0, num]), np.min(m_test))
+    _max = max(np.max(y_test[:, :, 0, num]), np.min(m_test))
+
+    # display perfect estimation
+    plt.subplot(142)
+    plt.title("perfect")
+    plt.imshow(y_test[:, :, 0, num], cmap='hot', vmin=_min, vmax=_max, extent=[0, 14, 0, 20])
+
+    # display model estimation
+    plt.subplot(143)
+    plt.title("model")
+    plt.imshow(m_test, cmap='hot', vmin=_min, vmax=_max, extent=[0, 14, 0, 20])
+
+    # display error (same range)
+    ax= plt.subplot(144)
+    #plt.imshow(m_test-y_test[:,:,0,num], cmap='hot', vmin = _min, vmax = _max, extent = [0, 14, 0, 20])
+    plt.title("hist abs diff")
+    er = np.abs((m_test-y_test[:,:,0,num]).numpy())
+    plt.hist(er.reshape([-1]), bins=30)
+
+    # display standard deviation of error
+    std_dev = np.std(er)
+    plt.text(0.99, 0.99, s=f'{std_dev:0.5}', ha='right', va='top', transform=ax.transAxes)
+
+    print(f'std:{std_dev}')
+
+    plt.show() # block=False)
